334f0421313bff337dd5000c014e8c7e841570f3
topi4k <55083186+topi4k@users.noreply.github.com>
2019-12-10 10:14:40 +0100

    Add files via upload

--- DataAnalysis/creation_baseMove.py
+++ DataAnalysis/creation_baseMove.py
@@ -28,8 +28,10 @@ import time
 #########################       PARAMETRES       ##############################
 ###############################################################################
 threshold_turn = 4
-threshold_turn_head = 3
 
+head_turn_value = 10 ### A PARAMETRER EN FONCTION DE LA CROIX
+plane_turn_value = 15 ### IDEM
+derive_cap = 2
 ###############################################################################
 #########################       CODE PRELIM       #############################
 ###############################################################################
@@ -43,8 +45,14 @@ DataMove = pd.DataFrame( columns = ['turning_plane','turning_head'])
 ############################     FONCTIONS     ################################
 ###############################################################################
 
-### AVEC LE LACET POUR LINSTANT !! PROCHAINE MESURE BIEN CHOISIR LES DATA DANS 100ms
+### AVEC LE LACET POUR LINSTANT (FD_AHRS_HEADING)!! PROCHAINE MESURE BIEN CHOISIR LES DATA DANS 100ms
 # Donne la direction du virage a chaque instant du vol.
+
+"""
+S'applique aux data de XPlane. Donne la direction du virage a chaque instant du vol. parametres a choisir ci-dessus (next update)
+"""
+
+
 
 def plane_turning(threshold_turn, df):
     
@@ -70,33 +78,108 @@ def plane_turning(threshold_turn, df):
     
         moyenne_deriv_cap = np.mean(deriv)
 
-        if (moyenne_roulis <= -15) & (moyenne_deriv_cap >= 2) :
-            DataMove.loc[t,'turning_plane'] = "<<<------"
+        if (moyenne_roulis <= - plane_turn_value) & (moyenne_deriv_cap >= derive_cap) :
+            DataMove.loc[t,'turning_plane'] = -1
+            #"<<<------"
             
-        elif (moyenne_roulis >= 15) & (moyenne_deriv_cap >= 2) :
-            DataMove.loc[t,'turning_plane'] = "------>>>"
+        elif (moyenne_roulis >= plane_turn_value) & (moyenne_deriv_cap >= derive_cap) :
+            DataMove.loc[t,'turning_plane'] = 1
+            #"------>>>"
         else:
-            DataMove.loc[t,'turning_plane'] = "^^^"
+            DataMove.loc[t,'turning_plane'] = 0
+            #"^^^"
     return(DataMove)
 
 
-
-def head_turning(threshold_turn_head, df):
+"""
+S'applique aux data de XPlane. Donne les instants ou la tête tourne a droite ou a gauche. head_turn_value reglee avec la croix (next update)
+"""
+def head_turning(df):
     cap_head = df.loc[:,["FD_PILOT_HEAD_HEADING"]]
     #cap_head_time = df.loc[:,["FD_TIME_MS"]]
     
     for t in range(len(df)):
            
-           if (cap_head.iloc[t].values <= -10) :
-                DataMove.loc[t,'turning_head'] = "<<<------"
+           if (cap_head.iloc[t].values <= -head_turn_value) :
+                DataMove.loc[t,'turning_head'] = -1
+                #"<<<------"
             
-           elif (cap_head.iloc[t].values >= 10) :
-                DataMove.loc[t,'turning_head'] = "------>>>"
+           elif (cap_head.iloc[t].values >= head_turn_value) :
+                DataMove.loc[t,'turning_head'] =  1
+                #"------>>>"
                 
            else:
-                DataMove.loc[t,'turning_head'] = "^^^"
+                DataMove.loc[t,'turning_head'] = 0
+                #"^^^"
     return(DataMove)
     
+  
+    
+"""
+S'applique directement à Datamove. grce aux data crees par les deux fonctions precedentes.
+Donne la frequence de tournee de tete pendant le virage.
+
+PAS D'ORIENTATION POUR L'INSTANT
+"""    
+def freq_head_turning(dm):
+    nb_virage = 0
+    nb_turn = 0
+    
+    turning_plane = []
+    turning_head = []
+    
+    turn_plane = dm.loc[:,["turning_plane"]]
+    turn_head = dm.loc[:,["turning_head"]]
+    
+    
+    for t in range(len(dm)-1):
+        
+        # NOUVEAU VIRAGE
+        if abs(turn_plane.iloc[t+1].values) == 1 and turn_plane.iloc[t].values == 0:
+            turning_plane.append(1)
+            
+            if abs(turn_head.iloc[t+1].values) == 1 :
+                turning_head.append([1])
+            else :
+                turning_head.append([0])
+                
+        # VIRAGE QUI CONTINUE
+        if abs(turn_plane.iloc[t+1].values) == 1 and turn_plane.iloc[t].values == 1:
+            turning_plane[nb_virage] += 1
+            
+            # LISTE DANS LA LISTE CONCERNANT LE VIRAGE EN COURS
+            if abs(turn_head.iloc[t+1].values) == 1 and turn_head.iloc[t].values == 1:
+                turning_head[nb_virage][nb_turn] += 1
+            if abs(turn_head.iloc[t+1].values) == 1 and turn_head.iloc[t].values == 0:
+                nb_turn += 1
+                turning_head[nb_virage].append(1)
+                
+        # FIN DE VIRAGE
+        if turn_plane.iloc[t+1].values == 0 and abs(turn_plane.iloc[t].values) == 1 : 
+            nb_virage += 1
+
+    Frequence_tete = []
+    
+    #une valeur toutes les 0.1sec dans le tableau
+    
+    for k in range(len(turning_head)):
+        Frequence_tete.append(100*len(turning_head[k])/turning_plane[k])
+
+
+    print("La premiere valeur est le nombre de virages, le premier tableau correspond a chacune de leur duree, le dernier au temps ou la tete est tournee. La derniere valeur correspond au ratio des deux dernieres")
+    return(nb_virage,turning_plane,turning_head,Frequence_tete) 
+    
+#turning_plane,turning_head)    
+    
+    
+    
+###############################################################################
+######################          TEST FONCTION          ########################  
+###############################################################################
     
 plane_turning(threshold_turn, dataFlight)
-head_turning(threshold_turn_head, dataFlight)
+head_turning(dataFlight)
+freq_head_turning(DataMove)
+
+
+### A CODER changement dinclinaison precede d'un regard (DEBUT ET FIN VIRAGE)
