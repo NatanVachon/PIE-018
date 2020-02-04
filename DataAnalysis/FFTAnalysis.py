ca6ba7b8695f600a49068bb1afd78fa95ca3b79a
NatanVachon <47736713+NatanVachon@users.noreply.github.com>
2020-02-04 16:38:12 +0100

    Clean fft

--- DataAnalysis/FFTAnalysis.py
+++ DataAnalysis/FFTAnalysis.py
@@ -5,18 +5,23 @@ Created on Wed Jan 29 10:45:12 2020
 @author: natan
 """
 
-#from scipy import signal
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import fft
+from scipy.signal import find_peaks
 
-name = "guilhem"
+name = "simon"
 data_path = "d:/natan/Documents/PIE/Logs/flight_10Dec2019_" + name
 data = pd.read_csv(data_path + "/numData_100ms.csv", sep=';')
 
-fs = 10 #Hz
-#x = data["FD_GYRO_Y"]
+print("Frequency analysis for " + name)
+
+fs = 10. #Hz
 x = data["FD_PILOT_HEAD_PITCH"]
+#x = data["FD_GYRO_Y"]
+f = np.linspace(-0.5 * fs, 0.5 * fs, len(x))
+N = len(x)
 
 # f, t, Sxx = signal.spectrogram(x, fs)
 # plt.pcolormesh(t, f, Sxx)
@@ -25,15 +30,19 @@ x = data["FD_PILOT_HEAD_PITCH"]
 # plt.show()
 
 # FFT
-xFFT = np.fft.fftshift(np.fft.fft(x))
-xFFT = xFFT[len(xFFT)//2:]
+xFFT = fft.fftshift(fft.fft(x))
+xFFT, f = xFFT[N//2:], f[N//2:] # Keep half of the fft
+xFFT = abs(xFFT) # Take modulus
+xFFT = xFFT / float(len(x)) #Normalization
 
-# First peak comparison
+# Peak finder
+peak_threshold = 0.8 * max(xFFT[1:])
+peakIndexes = find_peaks(xFFT[1:], height=peak_threshold)
+peakFrequencies, peakValues = [f[i] for i in peakIndexes[0]], [xFFT[i] for i in peakIndexes[0]]
+print("Peaks:", peakFrequencies)
 
-
-f = np.linspace(0., 5., len(xFFT))
 plt.figure()
-plt.plot(f[1:len(f)//5], abs(xFFT[1:len(xFFT)//5]))
+plt.plot(f[1:N//10], xFFT[1:N//10])
 plt.title(name)
 plt.xlabel("Frequency [Hz]")
 plt.ylabel("FFT modulus")
